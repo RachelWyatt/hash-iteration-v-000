0dd36875d2485bdd3b806c4c133e1a3a5e4343cd
Rachel Wyatt <rw126@evansville.edu>
2018-09-08 01:16:49 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kid, age|
-    puts "Happy birthday #{kid}! You are now #{age} years old!"
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
